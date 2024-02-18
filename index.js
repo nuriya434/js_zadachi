68f01eac2f257c8e9706d697e9ffbb78d8c1b295
nuriya <nuriya434@mail.ru>
2024-02-18 21:23:22 +0600

    zadachi

---
+++ index.js
@@ -0,0 +1,35 @@
+// let a = 1, b = 1;
+
+// let c = ++a; // 2
+// let d = b++; // 1
+// alert(c)
+// alert(d)
+
+
+// let a = 2;
+
+// let x = 1 + (a *= 2);//a=4, x=5
+// alert(a)
+// alert(x)
+
+// alert("" + 1 + 0) //10
+// alert("" - 1 + 0) //-1
+// alert(true + false) //1 
+// alert(6 / "3") //2
+// alert("2" * "3") //6
+// alert(4 + 5 + "px")//9px
+// alert("$" + 4 + 5) //$45
+// alert("4" - 2) //2
+// alert("4px" - 2) //2px NaN
+// alert("  -9  " + 5) //-4 (-9 5)
+// alert("  -9  " - 5)//-14 
+// console.log(null + 1) //1
+// alert(undefined + 1)//1 (NaN)
+// alert(" \t \n" - 2)//-2 
+
+
+
+let a = prompt("Первое число?", 1);
+let b = prompt("Второе число?", 2);
+
+alert(a + b); // 12
